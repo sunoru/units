1808268a698c39f5bb4992a507621bee302a80d8
sunoru <s@sunoru.com>
2014-01-28 23:09:33 +0800

    add data.py

---
+++ sciNote/units/data.py
@@ -0,0 +1,13 @@
+LENGTH_RATIOS = {
+    u'm':1e0, u'km':1e3, u'cm':1e-1, u'mm':1e-3,
+    u'µm':1e-6, u'nm':1e-9, u'pm':1e-12, u'fm':1e-15,
+    u'Å',
+}
+MASS = 2
+TIME = 3
+CURRENT = 4
+TEMPERATURE = 5
+AMOUNT = 6
+LUMINOUS = 7
+UNIT_RATIOS = {LENGTH:LENGTH_RATIOS, 
+
