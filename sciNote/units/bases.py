dead551eb4f66b5debd4c9dc602ab661b4f4ae15
sunoru <s@sunoru.com>
2014-01-29 13:30:09 +0800

    Unit's method

--- sciNote/units/bases.py
+++ sciNote/units/bases.py
@@ -3,25 +3,15 @@
 # by スノル
 
 from .data import *
+from .unit_error import (IlligalUnit, UnmatchedUnits)
 from .locals import *
 
 class BaseUnit():
-    @staticmethod
-    def genUnitnameInit():
-        re = []
-        for e1 in UNIT_DATA.values():
-            re.append(e1['default'])
-        return re
-
-    def __init__(self, unittype, unitname):
-        if unittype is None:
-            self.unittype = (0, ) * 7
+    def __init__(self, data):
+        if data is None:
+            self.data = {}
         else:
-            self.unittype = tuple(unittype)
-        if unitname is None:
-            self.unitname = tuple(BaseUnit.genUnitnameInit())
-        else:
-            self.unitname = tuple(unitname)
+            self.data = data
 
 class Unit(BaseUnit):
     '''Unit class.
@@ -50,7 +40,7 @@ class Unit(BaseUnit):
         ccflg = True
         censu = 1
         punit = [0] * 7
-        annit = BaseUnit.genUnitnameInit()
+        annit = [''] * 7
         miflg = False
         for i in xrange(0, len(mystr)):
             if mystr[i] == '^':
@@ -62,7 +52,7 @@ class Unit(BaseUnit):
                     censu = int(tmstr)
                     tmstr = mistr
                 if not Unit.findUnit(ccflg, tmstr, punit, censu, annit):
-                    raise Exception('Illigal Unit!')
+                    raise IlligalUnit()
                 tmstr = ''
                 ccflg = True
                 censu = 1
@@ -72,7 +62,7 @@ class Unit(BaseUnit):
                     censu = int(tmstr)
                     tmstr = mistr
                 if not Unit.findUnit(ccflg, tmstr, punit, censu, annit):
-                    raise Exception('Illigal Unit!')
+                    raise IlligalUnit()
                 tmstr = ''
                 ccflg = False
                 censu = 1
@@ -83,60 +73,119 @@ class Unit(BaseUnit):
             censu = int(tmstr)
             tmstr = mistr
         if not Unit.findUnit(ccflg, tmstr, punit, censu, annit):
-            raise Exception('Illigal Unit!')
+            raise IlligalUnit()
         return punit, annit
 
     @staticmethod
     def genUnit(name):
         u1, u2 = Unit.analysis(name)
-        return Unit(u1, u2)
+        re = {i1:(u1[i1], u2[i1]) for i1 in xrange(0, len(u2)) if u1[i1]!=0}
+        return Unit(re)
 
-    def __init__(self, unittype=None, unitname=None):
-        BaseUnit.__init__(self, unittype, unitname)
+    def __init__(self, data=None):
+        BaseUnit.__init__(self, data)
 
     def __repr__(self):
         re = ''
-        for e1 in xrange(0, len(self.unittype)):
-            if self.unittype[e1] == 0:
-                continue
-            elif self.unittype[e1] == 1:
-                re += '*' + self.unitname[e1]
+        for e1 in  self.data.values():
+            if e1[0] == 1:
+                re += '*' + e1[1]
             else:
-                re += '*' + self.unitname[e1] + '^' + str(self.unittype[e1])
+                re += '*' + e1[1] + '^' + e1[0]
         return re[1:]
 
-    def __add__(self, other):
-        if self.unittype != 
+    def copy(self):
+        return Unit(self.data)
 
+    def normalize(self):
+        ratio = 1e0
+        for e1, e2 in self.data.items():
+            ratio *= UNIT_DATA[e1][e2[1]]
+            self.data[e1] = (e2[0], UNIT_DATA[e1]['default'])
+        return ratio
+
+    def convert(self, other):
+        re = self.copy()
+        ra = 1e0
+        for e1 in self.data.keys():
+            ra *= other.data.keys()
+
+
+    def __eq__(self, other):
+        p1 = self.data.keys()
+        p2 = other.data.keys()
+        if p1 != p2:
+            return False
+        for e1 in xrange(0, len(p1)):
+            if self.data[p1[e1]][0] != other.data[p2[e1]][0]:
+                return False
+        return True
+
+    def __ne__(self, other):
+        return not (self == other)
+
+    def __add__(self, other):
+        if self != other:
+            raise UnmatchedUnits()
+        return self.copy()
+
+    def __sub__(self, other):
+        if self != other:
+            raise UnmatchedUnits()
+        return self.copy()
+
+    def __mul__(self, other):
+        it = other.data.items()
+        re = self.copy()
+        for i1 in xrange(0, len(it)):
+            if not re.data.has_key(it[i1][0]):
+                re.data[it[i1][0]] = it[i1][1]
+            tmp = re.data[it[i1][0]][0] + it[i1][1][0]
+            if tmp == 0:
+                del re.data[it[i1][0]]
+            else
+                re.data[it[i1][0]] = (tmp, re.data[it[i1][0]][1])
+        return re,
+
+    def __div__(self, other):
+        it = other.data.items()
+        re = self.copy()
+        for i1 in xrange(0, len(it)):
+            if not re.data.has_key(it[i1][0]):
+                re.data[it[i1][0]] = -it[i1][1]
+            tmp = re.data[it[i1][0]][0] - it[i1][1][0]
+            if tmp == 0:
+                del re.data[it[i1][0]]
+            else
+                re.data[it[i1][0]] = (tmp, re.data[it[i1][0]][1])
+        return re,
 
 class ValueUnit(float):
-    def __init__(self, value, unit):
-        float.__init__(self, value)
-        self.unit = unit
-        
+
     @staticmethod
     def convert(value, unit_before, unit_after):
         if unit_before is unit_after:
             return value
         return value * unit_after.ratio() / unit_before.ratio()
 
+    def __init__(self, value, unit):
+        float.__init__(self, value)
+        self.unit = unit
+
     def __abs__(self):
         return ValueUnit(float.__abs__(self), self.unit)
         
     def __add__(self, other):
-        if self.unit.unittype != self.other.unittype:
-            raise Exception('Different Type of Data')
-# TODO
-        self
+        self.unit += other.unit 
+##
         return ValueUnit(float.__add__(self, other), self.unit)
         
     def __div__(self, other):
-        if self.unit.unittype != self.other.unittype:
-            raise Exception('Different Type of Data')
+        self.unit
         return ValueUnit(float.__div__(self, other))
         
     def __divmod__(self, other):
-        if self.unit.unittype != self.other.unittype:
+        if self.unit.data != self.other.data:
             raise Exception('Different Type of Data')
 
         return ValueUnit(float.__add__(self, other))
