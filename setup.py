c39e0c459cd112ec9000a4fde00ae31f2ca67f2a
sunoru <s@sunoru.com>
2014-01-31 21:07:26 +0800

    setup bug

--- setup.py
+++ setup.py
@@ -10,14 +10,14 @@ sciNote_init = join('src', 'units', '__init__.py')
 exec(compile(open(sciNote_init).read(), sciNote_init, 'exec'), d)
 
 setup(
-    name = 'sciNote',
+    name = 'units',
     version = d['__version__'],
     author = u'スノル',
     author_email = 's@sunoru.com',
-    url = 'https://github.com/sunoru/sciNote',
+    url = 'https://github.com/sunoru/units',
     description = 'Use Python for scientific calculation.',
     long_description = open('README.md').read(),
-    download_url = ('https://www.sunoru.com/code/sciNote-%s' %
+    download_url = ('https://www.sunoru.com/code/units-%s' %
         d['__version__']),
     include_package_data = True,
     license = 'Apache license 2.0',
